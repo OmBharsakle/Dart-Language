9afe8c3907b5d00028967376dde225a79b445542
OmBharsakle <149371245+OmBharsakle@users.noreply.github.com>
2024-03-12 15:45:38 +0530

    Update 02.dart

--- 02. Fundamentals Of Dart/Lecture 2 - Lab Work/02.dart
+++ 02. Fundamentals Of Dart/Lecture 2 - Lab Work/02.dart
@@ -1,4 +1,20 @@
+import 'dart:io';
+
 void main()
 {
-    List l1 
+  List l1= [];
+  int i;
+
+  for(i=0; i<28; i++)
+  {
+    stdout.write("${i+1}.Entre The Indian Sates Name : ");
+    String name = stdin.readLineSync()!;
+    l1.add(name);
+  }
+
+  l1.forEach((element) 
+  {
+    print(element);
+  });
+  
 }
