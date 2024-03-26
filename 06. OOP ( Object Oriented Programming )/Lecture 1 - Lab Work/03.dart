53aee09ce97eeec3548d312a952ced5d1f201e95
OmBharsakle <149371245+OmBharsakle@users.noreply.github.com>
2024-03-26 11:42:07 +0530

    Update 03.dart

--- 06. OOP ( Object Oriented Programming )/Lecture 1 - Lab Work/03.dart
+++ 06. OOP ( Object Oriented Programming )/Lecture 1 - Lab Work/03.dart
@@ -6,137 +6,135 @@
 
 import 'dart:io';
 
-class Supermarket {
-  int? ItemNumber, Quantity, Tax, Discount, choice;
-  String? ItemName;
+class SuperMarket {
+  int? _ItemNumber, _Quantity, _Tax, _Discount, _Choice;
+  String? _ItemName;
 
-  void setter() {
-    stdout.write("Enter Item Details : ${i+1} \n");
+  void setter(int i) {
+    stdout.write("Enter Item Details Number : ${i+1} \n");
     stdout.write("--------------------------------\n");
-    stdout.write("Enter the Item Number : ");
-    ItemNumber = int.parse(stdin.readLineSync()!);
-    stdout.write("Enter The Item Name : ");
-    ItemName = stdin.readLineSync()!;
-    stdout.write("Enter The Quantity : ");
-    Quantity = int.parse(stdin.readLineSync()!);
-    stdout.write("Enter The Tax : ");
-    Tax = int.parse(stdin.readLineSync()!);
-    stdout.write("Enter The Discount : ");
-    Discount = int.parse(stdin.readLineSync()!);
+    stdout.write("Enter Item Number : ");
+    _ItemNumber = int.parse(stdin.readLineSync()!);
+    stdout.write("Enter Item Name : ");
+    _ItemName = stdin.readLineSync()!;
+    stdout.write("Enter Item Quantity : ");
+    _Quantity = int.parse(stdin.readLineSync()!);
+    stdout.write("Enter Item Tax : ");
+    _Tax = int.parse(stdin.readLineSync()!);
+    stdout.write("Enter Item Discount : ");
+    _Discount = int.parse(stdin.readLineSync()!);
     stdout.write("--------------------------------\n");
     stdout.writeln();
   }
 
-  void getter() {
-    stdout.write("\nItem Details Are Below : \n");
+  void getter(int i) {
+    stdout.write("\nItem Details Are Below : ${i+1} \n");
     stdout.write("--------------------------------\n");
-    stdout.write("Item Number : $ItemNumber\n");
-    stdout.write("Item Name : $ItemName\n");
-    stdout.write("Quantity : $Quantity\n");
-    stdout.write("Tax : $Tax %\n");
-    stdout.write("Discount : $Discount %\n");
+    stdout.write("Item Number : $_ItemNumber\n");
+    stdout.write("Item Name : $_ItemName\n");
+    stdout.write("Item Quantity : $_Quantity\n");
+    stdout.write("Item Tax : $_Tax %\n");
+    stdout.write("Item Discount : $_Discount %\n");
     stdout.write("--------------------------------\n");
   }
 
   void GetChoice() {
-    print("\n\nWelcome To SuperMarket Billing System");
+    print("Welcome To SuperMarket Billing System");
     print("--------------------------------");
-    print(" 1 For Add Item Details");
-    print(" 2 For Display All Item Details");
-    print(" 3 For Search By Item Number");
-    print(" 4 For Exit");
-    print("--------------------------------");
+    print("1. For Add Item Details");
+    print("2. For Display All Item Details");
+    print("3. For Search By Item Number");
+    print("4. For Exit");
+    print("--------------------------------\n");
     stdout.write("Enter Your Choice : ");
-    choice = int.parse(stdin.readLineSync()!);
+    _Choice = int.parse(stdin.readLineSync()!);
   }
 }
 
 void main() {
   int i, n, j;
-  String id, pswrd;
-  bool check = false;
-  List<Supermarket> list = [];
-  Supermarket obj = new Supermarket();
+  String id, pass;
+  List<SuperMarket> list = [];
+  SuperMarket obj = new SuperMarket();
 
-  stdout.write("\nUser ID : admin\nPassword : 1234\n\n");
+  stdout.write("\nUser ID : Admin\nPassword : 1234\n\n");
 
-  // Verify User ID And Password
-  do {
-    stdout.write("Enter Your User ID : ");
-    id = stdin.readLineSync()!;
-    stdout.write("Enter Your Password : ");
-    pswrd = stdin.readLineSync()!;
-    if (id == "admin" && pswrd == "1234") {
-      stdout.write("\nLogin Successfully..\n");
-      check = true;
-    } else {
-      stdout.write("\nInvalid User ID or Password..\n");
-    }
-  } while (!check);
-
-  stdout.write("\nEnter the N number of Item : ");
-  n = int.parse(stdin.readLineSync()!);
-
-  for (i = 0; i < n; i++) {
-    Supermarket obj = Supermarket();
-    obj.setter();
-    list.add(obj);
+  stdout.write("Enter Your User ID : ");
+  id = stdin.readLineSync()!;
+  stdout.write("Enter Your Password : ");
+  pass = stdin.readLineSync()!;
+  if (id == "Admin" && pass == "1234") 
+  {
+    stdout.write("\nLogin Successfully...\n");
+    stdout.write("\nEnter The Number Of Item : ");
+   n = int.parse(stdin.readLineSync()!);
+ 
+   for (i = 0; i < n; i++) 
+   {
+     SuperMarket obj = SuperMarket();
+     obj.setter(i);
+     list.add(obj);
+   }
+ 
+   do {
+     obj.GetChoice();
+     switch (obj._Choice) 
+     {
+       case 1:
+         stdout.write("How Many Items You Want To Add : ");
+         int addItem = int.parse(stdin.readLineSync()!);
+         for (i = 0; i < addItem; i++) 
+         {
+           SuperMarket obj = SuperMarket();
+           obj.setter(i);
+           list.add(obj);
+         }
+         print("Item Add Successfully..");
+         break;
+ 
+       case 2:
+         for (i = 0; i < list.length; i++) 
+         {
+           for (j = i + 1; j < list.length; j++) 
+           {
+             if (list[i]._ItemNumber! > list[j]._ItemNumber!) 
+             {
+               SuperMarket obj = list[j];
+               list[j] = list[i];
+               list[i] = obj;
+             }
+           }
+         }
+         print("\nDisplay All Record In Ascending Order : ");
+         for (i = 0; i < list.length; i++) {
+           list[i].getter(i);
+         }
+         break;
+ 
+       case 3:
+         int track = 0;
+         stdout.write("Enter the Item Number : ");
+         int number = int.parse(stdin.readLineSync()!);
+         for (i = 0; i < list.length; i++) {
+           if (list[i]._ItemNumber == number) {
+             list[i].getter(i);
+             track = 1;
+           }
+         }
+         if (track != 1) {
+           stdout.write("\nItem's Are Not Found..\n");
+         }
+         break;
+ 
+       case 4:
+         stdout.write("\nThank You For Visit..\n");
+         break;
+ 
+       default:
+         stdout.write("\nEnter Valid Choice (1,2,3,4)\n");
+     }
+   } while (obj._Choice != 4);
+  } else {
+    stdout.write("\nInvalid User ID Or Password..\n");
   }
-
-  //Railway Reservation System
-  do {
-    obj.GetChoice();
-    switch (obj.choice) {
-      case 1:
-        stdout.write("How Many Items You Want to Add : ");
-        int c = n, addItem = int.parse(stdin.readLineSync()!);
-        for (i = 0; i < addItem; i++) {
-          Supermarket obj = Supermarket();
-          obj.setter();
-          list.add(obj);
-          c++;
-        }
-        print("Item Add Successfully..");
-        break;
-
-      case 2:
-        //Sort In Ascending Order
-        for (i = 0; i < list.length; i++) {
-          for (j = i + 1; j < list.length; j++) {
-            if (list[i].ItemNumber! > list[j].ItemNumber!) {
-              Supermarket obj = list[j];
-              list[j] = list[i];
-              list[i] = obj;
-            }
-          }
-        }
-        print("\nAll Train Details in Ascending Order Are Below : ");
-        for (i = 0; i < list.length; i++) {
-          list[i].getter();
-        }
-        break;
-
-      case 3:
-        bool found = false;
-        stdout.write("Enter the Item Number : ");
-        int number = int.parse(stdin.readLineSync()!);
-        for (i = 0; i < list.length; i++) {
-          if (list[i].ItemNumber == number) {
-            list[i].getter();
-            found = true;
-          }
-        }
-        if (found != true) {
-          stdout.write("\nItem Not Found..\n");
-        }
-        break;
-
-      case 4:
-        stdout.write("\nThank You For Visit..\n");
-        break;
-
-      default:
-        stdout.write("\nEnter Valid Choice(1,2,3,4)..!!\n");
-    }
-  } while (obj.choice != 4);
 }
